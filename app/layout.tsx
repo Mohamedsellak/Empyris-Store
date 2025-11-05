4a24cabf7a878a7c13edb117c9a9bc44e5d9613a
unknown <mohamed._.sellak@hotmail.com>
2025-11-05 15:26:02 +0100

    Update TikTok Pixel ID to D45LQN3C77U6O1UKPAEG

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
               var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
               ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
               
-              ttq.load('D41JC0BC77U40Q0I64JG');
+              ttq.load('D45LQN3C77U6O1UKPAEG');
               ttq.page();
               }(window, document, 'ttq');
             `,
